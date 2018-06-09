3498348d77a464a3f68f0036a3c95c73f6b088bf
wang-man <370794742@qq.com>
2018-06-09 18:29:52 +0800

    使用react-redux改写redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addGun, reduceGun, reduceGunAsync} from './index.redux'
 
 class App extends React.Component{
   render(){
-    const store = this.props.store    // 读取父组件传递来的store
-    const mum = store.getState()      // 获取状态
-    console.log(store)
+    const num = this.props.num
     const addGun = this.props.addGun
     const reduceGun = this.props.reduceGun
     const reduceGunAsync = this.props.reduceGunAsync
     return (
       <div>
-        <h1>现在有机枪{mum}把</h1>
-        <button onClick={()=>store.dispatch(addGun())}>申请武器</button>
-        <button onClick={()=>store.dispatch(reduceGun())}>上交武器</button>
-        <button onClick={()=>store.dispatch(reduceGunAsync())}>拖两天在上交</button>
+        <h1>现在有机枪{num}把</h1>
+        <button onClick={addGun}>申请武器</button>
+        <button onClick={reduceGun}>上交武器</button>
+        <button onClick={reduceGunAsync}>拖两天在上交</button>
       </div>
     ) 
   }
 }
 
+const mapStateToProps = state=>{
+  return {num: state}     // 将状态给到num变量
+}
+const actionCreators = {addGun, reduceGun, reduceGunAsync}
+// 将state、addGun、reduceGun、reduceGunAsync也塞进props中，这样就可以使用this.props使用
+App = connect(mapStateToProps, actionCreators)(App)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
 import App from './App'
 import {counter, addGun, reduceGun, reduceGunAsync} from './index.redux'
 
@@ -10,11 +11,9 @@ const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : ()
 // 创建store,注意用createStore
 const store = createStore(counter, compose(applyMiddleware(thunk), reduxDevtools)) 
 
-function render () {					// 使用父子组件传值的方式传递
-	ReactDom.render(<App store={store} addGun={addGun} reduceGun={reduceGun} reduceGunAsync={reduceGunAsync} />, document.getElementById('root'))
-}
-
-render()
-
-// 一定要调用subscribe订阅才可以监听到数据变化
-store.subscribe(render)
+ReactDom.render(
+	(<Provider store={store}>
+		<App/>
+	</Provider>),
+	document.getElementById('root')
+)
